c2569c9bb808e3b1753353d134be2337c75dde6b
tomekb530 <tomekb530@gmail.com>
2025-02-06 19:19:55 +0100

    Addons Update

--- update.sh
+++ update.sh
@@ -1,1 +1,5 @@
-git submodule -q foreach git pull
+#!/bin/bash
+
+git submodule -q foreach "(git checkout master || true) && (git checkout main || true) && git pull"
+git add .
+git commit -m "Addons update"
